d72436fca56e3363566d5c703f20087fb2c0d912
tylerae <84347788+tylerae@users.noreply.github.com>
2023-12-04 19:09:37 -0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,55 @@
+// setup comments
 const comments = document.querySelector('.guides');
 
-// setup comments
 const setupComments = (data) => {
-  let html = '';
-  Object.entries(data).forEach(entry => {
-    const [key, value] = entry;
-    const li = `
-      <li>
-        <div class="collapsible-header grey lighten-4">${key}</div>
-        <div class="collapsible-body white">${value}</div>
-      </li>
-    `;
-    html += li;
+
+  if (data) {
+    let html = '';
+    Object.entries(data).forEach(entry => {
+      const [key, value] = entry;
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4">Click to view ${key}'s comment.</div>
+          <div class="collapsible-body white">${value}</div>
+        </li>
+      `;
+      html += li;
+    });
+    comments.innerHTML = html;
+  }else{comments.innerHTML = '<h5 class="center-align">Please sign in or create an account</h5>';}
+}
+
+
+// handle logged in and out user NAVBAR
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+
+const setupUI = (user) => {
+  if (user) {
+    // toggle user UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  }
+  else {
+    // toggle user elements
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  } 
+};
+
+
+
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+  
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+  
   });
-  comments.innerHTML = html;
-}
-
-const ouputNotSignedIn = document.querySelector('.random-out');
-
-const isLoggedOut = () => {
-  ouputNotSignedIn.innerHTML = `
-    <div class="collapsible-header gret lighten-4">
-      <div>You Need to sign in to view comments. If you would like to skip login and go straight to the Portfolio Web Site, click the Portfolio tab above.</div>
-    </div>
-  `;
-}
 
 
 // setup materialize components
